12ec566d759825eaaa118fbb7aaecd7b4cb572fe
Danilo A. Silva <danilo.official22@hotmail.com>
2025-02-10 16:04:33 -0300

    creating a basic structure of backend

---
+++ backend/src/router/routes/routes.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"net/http"
+
+	pingController "github.com/Dan0Silva/my_rooms/src/controller"
+	"github.com/gorilla/mux"
+)
+
+type Routes struct {
+	Uri 					string
+	Method 				string
+	Function 			func(http.ResponseWriter, *http.Request)
+	RequireAuth 	bool
+}
+
+var pingRoute = Routes{
+	Uri: "/ping",
+	Method: http.MethodGet,
+	Function: pingController.Ping,
+	RequireAuth: false,
+}
+
+func GenerateRoutes(router *mux.Router) {
+	allroutes := []Routes{}
+
+	allroutes = append(allroutes, pingRoute)
+
+	for _, route := range allroutes {
+		router.HandleFunc(route.Uri, route.Function).Methods(route.Method)
+	}
+}
