240f9daa5ab18cc16f083a2ca8b14e694191b040
Danilo A. Silva <danilo.official22@hotmail.com>
2025-02-11 15:22:01 -0300

    creating a default response module

--- backend/src/controller/spaceController.go
+++ backend/src/controller/spaceController.go
@@ -1,23 +1,30 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func CreateSpace(w http.ResponseWriter, h *http.Request) {
-	
+func CreateSpace(w http.ResponseWriter, r *http.Request) {
+	// var newSpace models.Space
+
+	// reqBody, err := io.ReadAll(r.Body)
+	// if err != nil {
+
+	// }
 }
 
-func ListSpaces(w http.ResponseWriter, h *http.Request) {
+func ListSpaces(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func ViewSpace(w http.ResponseWriter, h *http.Request) {
+func ViewSpace(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func EditSpace(w http.ResponseWriter, h *http.Request) {
+func EditSpace(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func DeleteSpace(w http.ResponseWriter, h *http.Request) {
+func DeleteSpace(w http.ResponseWriter, r *http.Request) {
 
 }
